d05630f6efdb2b8d8947cd504ec72fcfcc9d8b91
Diederia <d.w.calkoen@student.vu.nl>
2016-11-29 09:30:53 +0100

    Initial Commit

---
+++ Diederick-Calkoen-pset5/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Diederick-Calkoen-pset5
+//
+//  Created by Diederick Calkoen on 29/11/16.
+//  Copyright © 2016 Diederick Calkoen. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
